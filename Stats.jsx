9ec338487eb3704ada7f9687ed75a10fc8bf7ec6
Prashanth-2002 <103819382+Prashanth-2002@users.noreply.github.com>
2024-03-08 11:39:27 +0530

    Update Stats.jsx

--- Stats.jsx
+++ Stats.jsx
@@ -1,20 +1,12 @@
 // Stats.jsx
-import React from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
+
 import './stats.css';
 import CountsImg from '../../components/stats/counts-img.jpg';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Stats = () => {
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
+ 
 
   return (
     <div className="container flex-container">
@@ -23,7 +15,7 @@ const Stats = () => {
       </div>
       <div className="paragraphs-container">
         <h1 className="header">Happy Customers</h1>
-        <Slider {...sliderSettings}>
+        
           <div>
             <div className="first wide-container">
               <i className="bi bi-emoji-smile"></i>
@@ -41,7 +33,7 @@ const Stats = () => {
             </div>
           </div>
           {/* Add more slides as needed */}
-        </Slider>
+       
       </div>
     </div>
   );
